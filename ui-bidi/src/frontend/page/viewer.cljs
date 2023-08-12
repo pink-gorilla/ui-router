8e56f2c56e3e84348d9c881a0b7ff7a06f723c9d
awb99 <awb99@gmail.com>
2023-08-11 19:57:18 -0500

    page viewer

---
+++ ui-bidi/src/frontend/page/viewer.cljs
@@ -0,0 +1,21 @@
+(ns frontend.page.viewer
+  (:require
+   [reagent.core :as r]
+   [re-frame.core :refer [subscribe]]
+   [frontend.page :refer [reagent-page]]
+   [frontend.routes :refer [current]]))
+
+(defonce generation (r/atom 1))
+
+(defn page-viewer []
+  (fn []
+    ^{:key [@generation @current]}
+    [reagent-page @current]) ; multimethod fix
+
+(defn refresh-page
+  "used in goldly for dynamic reloading when page source was changed"
+  []
+  (swap! generation inc))
+
+; https://stackoverflow.com/questions/33299746/why-are-multi-methods-not-working-as-functions-for-reagent-re-frame
+; ^{:key @current-route} [pages @current-route]
