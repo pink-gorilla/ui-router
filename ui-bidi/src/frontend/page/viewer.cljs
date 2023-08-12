ae1e471596fa5b28bb9a8d3b67151e68538e17a5
awb99 <awb99@gmail.com>
2023-08-12 13:02:41 -0500

    page-viewer bugfix

--- ui-bidi/src/frontend/page/viewer.cljs
+++ ui-bidi/src/frontend/page/viewer.cljs
@@ -7,15 +7,17 @@
 
 (defonce generation (r/atom 1))
 
+; https://stackoverflow.com/questions/33299746/why-are-multi-methods-not-working-as-functions-for-reagent-re-frame
+; ^{:key @current-route} [pages @current-route]
+
 (defn page-viewer []
   (fn []
     ^{:key [@generation @current]}
-    [reagent-page @current]) ; multimethod fix
+    [reagent-page @current])) ; multimethod fix
 
 (defn refresh-page
   "used in goldly for dynamic reloading when page source was changed"
   []
   (swap! generation inc))
 
-; https://stackoverflow.com/questions/33299746/why-are-multi-methods-not-working-as-functions-for-reagent-re-frame
-; ^{:key @current-route} [pages @current-route]
+
